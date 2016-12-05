9aadad0c56fa0234c05e4a565869c8297b056ca3
yasinfp <yfpayalan@gmail.com>
2016-12-05 23:57:07 +0300

    sound capture class added

--- app/src/main/java/com/firat/noiseapp/ScreenDetectBReciever.java
+++ app/src/main/java/com/firat/noiseapp/ScreenDetectBReciever.java
@@ -20,29 +20,19 @@ import java.io.IOException;
  */
 public class ScreenDetectBReciever extends BroadcastReceiver {
     public static final String TAG = "ScreenDetectBtReceiver";
-    private AudioRecord audioRecord =null;
-    private int bufferSize = 0;
-    private boolean isRecording = false;
-    private Thread recordingThread = null;
-    public static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
-    public static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
-    public static final String AUDIO_RECORDER_FILE_EXT_WAV=".wav";
-    public static final int RECORDER_BPP = 16;
-
+    public static final int RECORD_TIME=1000*5;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         try
         {
-            bufferSize = AudioRecord.getMinBufferSize(8000
-                    , AudioFormat.CHANNEL_IN_STEREO
-                    , AudioFormat.ENCODING_PCM_16BIT);
+            SoundCapture soundCapture = new SoundCapture();
             if (intent.getAction().equals(Intent.ACTION_USER_PRESENT))
             {
                 Log.d(TAG, "screen unlocked");
-               startRecording(context);
-                Thread.sleep(10000);
-               stopRecording(context);
+                soundCapture.startRecording(context);
+                Thread.sleep(RECORD_TIME);
+                soundCapture.stopRecording(context);
             }
         }
         catch (Exception e)
@@ -51,206 +41,5 @@ public class ScreenDetectBReciever extends BroadcastReceiver {
 
         }
     }
-    private void startRecording(final Context context)
-    {
-        Log.d(TAG, "start recording");
 
-        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC
-                                    ,44100
-                                    ,AudioFormat.CHANNEL_IN_STEREO
-                                    ,AudioFormat.ENCODING_PCM_16BIT
-                                    ,bufferSize);
-        int recorderState = audioRecord.getState();
-        if (recorderState == 1)
-            audioRecord.startRecording();
-
-        isRecording = true;
-        recordingThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                writeAudioDataToFile(context);
-            }
-        },"Recording Thread");
-
-        recordingThread.start();
-    }
-    private void stopRecording(Context context)
-    {
-        Log.d(TAG, "stop recording");
-
-        if ( audioRecord!=null)
-        {
-            isRecording = false;
-        }
-        if (audioRecord.getState() == 1)
-        {
-            audioRecord.stop();
-            audioRecord.release();
-        }
-        audioRecord = null;
-        recordingThread = null;
-
-        copyWavFile(getTempFilename(context), getFilename(context));
-        deleteTempFile(context);
-    }
-
-    private void deleteTempFile(Context context)
-    {
-        File file = new File(getTempFilename(context));
-        file.delete();
-    }
-
-    private void copyWavFile(String inFileName, String outFileName)
-    {
-        try
-        {
-            FileInputStream fin = null;
-            FileOutputStream fos = null;
-            long totalAudioLen = 0;
-            long totalDataLen = totalAudioLen + 36;
-            long longSampleRate = 44100;
-            int channels = 2;
-            long byteRate = RECORDER_BPP * 44100 * channels / 8;
-
-            byte[] data = new byte[bufferSize];
-            fin = new FileInputStream(inFileName);
-            fos = new FileOutputStream(outFileName);
-
-            totalAudioLen = fin.getChannel().size();
-            totalDataLen = totalAudioLen + 36;
-            Log.i(TAG,"File size= "+totalDataLen);
-            writeWavFileHeader(fos,totalAudioLen,totalDataLen,longSampleRate,channels,byteRate);
-            while (fin.read(data) != -1)
-            {
-                fos.write(data);
-            }
-
-            fin.close();
-            fos.close();
-        }
-        catch (Exception e)
-        {
-            Log.e(TAG,e.getMessage(),e.fillInStackTrace());
-        }
-
-    }
-
-    private void writeWavFileHeader(FileOutputStream fos, long totalAudioLen, long totalDataLen, long longSampleRate, int channels, long byteRate) throws IOException {
-        byte[] header = new byte[44];
-        header[0] = 'R'; // RIFF/WAVE header
-        header[1] = 'I';
-        header[2] = 'F';
-        header[3] = 'F';
-        header[4] = (byte) (totalDataLen & 0xff);
-        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
-        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
-        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
-        header[8] = 'W';
-        header[9] = 'A';
-        header[10] = 'V';
-        header[11] = 'E';
-        header[12] = 'f'; // 'fmt ' chunk
-        header[13] = 'm';
-        header[14] = 't';
-        header[15] = ' ';
-        header[16] = 16; // 4 bytes: size of 'fmt ' chunk
-        header[17] = 0;
-        header[18] = 0;
-        header[19] = 0;
-        header[20] = 1; // format = 1
-        header[21] = 0;
-        header[22] = (byte) channels;
-        header[23] = 0;
-        header[24] = (byte) (longSampleRate & 0xff);
-        header[25] = (byte) ((longSampleRate >> 8) & 0xff);
-        header[26] = (byte) ((longSampleRate >> 16) & 0xff);
-        header[27] = (byte) ((longSampleRate >> 24) & 0xff);
-        header[28] = (byte) (byteRate & 0xff);
-        header[29] = (byte) ((byteRate >> 8) & 0xff);
-        header[30] = (byte) ((byteRate >> 16) & 0xff);
-        header[31] = (byte) ((byteRate >> 24) & 0xff);
-        header[32] = (byte) (2 * 16 / 8); // block align
-        header[33] = 0;
-        header[34] = RECORDER_BPP; // bits per sample
-        header[35] = 0;
-        header[36] = 'd';
-        header[37] = 'a';
-        header[38] = 't';
-        header[39] = 'a';
-        header[40] = (byte) (totalAudioLen & 0xff);
-        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
-        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
-        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
-
-        fos.write(header,0,44);
-    }
-
-
-
-
-    private void writeAudioDataToFile(Context context)
-    {
-        byte data[] = new byte[bufferSize];
-        String fileName = getTempFilename(context);
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(fileName);
-        }
-        catch (FileNotFoundException e)
-        {
-            Log.e(TAG,e.getMessage(),e.fillInStackTrace());
-        }
-
-        int read = 0;
-
-        if (null != fos)
-        {
-            while (isRecording)
-            {
-                read = audioRecord.read(data,0,bufferSize);
-                if (AudioRecord.ERROR_INVALID_OPERATION != read)
-                {
-                    try {
-                        fos.write(data);
-                    }
-                    catch (IOException e)
-                    {
-                        Log.e(TAG,e.getMessage(),e.fillInStackTrace());
-                    }
-                }
-            }
-        }
-    }
-
-    private String getTempFilename(Context context)
-    {
-        String filepath = Environment.getExternalStorageDirectory().getPath();
-//        String filepath = context.getFilesDir().getPath();
-        File file = new File(filepath,AUDIO_RECORDER_FOLDER);
-
-        if(!file.exists())
-        {
-            file.mkdirs();
-        }
-
-        File tempFile = new File(filepath,AUDIO_RECORDER_TEMP_FILE);
-        if (tempFile.exists())
-        {
-            tempFile.delete();
-        }
-
-        return (file.getAbsolutePath()+"/"+AUDIO_RECORDER_TEMP_FILE);
-    }
-
-    private String getFilename(Context context)
-    {
-        String filepath = Environment.getExternalStorageDirectory().getPath();
-//        String filepath = context.getFilesDir().getPath();
-        File file = new File(filepath,AUDIO_RECORDER_FOLDER);
-        Log.d(TAG,"File location = "+context.getCacheDir()+"/"+AUDIO_RECORDER_FOLDER);
-        if (!file.exists())
-            file.mkdirs();
-
-        return (file.getAbsolutePath()+"/"+System.currentTimeMillis()+AUDIO_RECORDER_FILE_EXT_WAV);
-    }
 }
